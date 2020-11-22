aa417b5035334e037108b0c5ee7fb82996167b66
unknown <you@example.com>
2020-11-22 11:27:44 +0700

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,124 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <title>SMK IKHLAS JAWILAN</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('plugins/toastr/toastr.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }}">
+  <link rel="shrotcut icon" href="{{ asset('img/favicon.ico') }}">
+</head>
+<body class="hold-transition login-page" style="background-image: url('{{ asset("img/wallup.jpg") }}'); background-size: cover;">
+
+  <div class="login-box" style="border-radius: 10px;">
+    <div class="login-logo" style="color: white; text-shadow: 2px 2px 4px #000000">
+      <img src="{{ asset('img/favicon_logo.png') }}" width="20%" alt="gambar belum terload">
+      <h2 class="pt-4 mb-5"><strong>SMK IKHLAS JAWILAN</strong></h2>
+    </div>
+
+    <div class="card" style="border-radius: 15px;">
+      @yield('content')
+    </div>
+
+    <footer style="color: white;">
+      <marquee>
+          <strong>Copyright &copy; <script>document.write(new Date().getFullYear());</script> &diams; <a href="http://smkn1jenpo.sch.id/" style="color: white;">SMK IKHLAS JAWILAN</a>. </strong>
+      </marquee>
+    </footer>
+  </div>
+  
+
+<!-- jQuery -->
+<script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('plugins/toastr/toastr.min.js') }}"></script>
+<!-- page script -->
+<script>
+  $(document).ready(function(){
+      $('#role').change(function(){
+          var kel = $('#role option:selected').val();
+          if (kel == "Guru") {
+            $("#noId").addClass("mb-3");
+            $("#noId").html(`
+              <input id="nomer" type="text" maxlength="5" onkeypress="return inputAngka(event)" placeholder="No Id Card" class="form-control @error('nomer') is-invalid @enderror" name="nomer" autocomplete="nomer">
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <span class="fas fa-id-card"></span>
+                </div>
+              </div>
+              `);
+            $("#pesan").html(`
+              @error('nomer')
+                <span class="invalid-feedback" role="alert">
+                  <strong>{{ $message }}</strong>
+                </span>
+              @enderror
+            `);
+          } else if(kel == "Siswa") {
+            $("#noId").addClass("mb-3");
+            $("#noId").html(`
+              <input id="nomer" type="text" placeholder="No Induk Siswa" class="form-control" name="nomer" autocomplete="nomer">
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <span class="fas fa-id-card"></span>
+                </div>
+              </div>
+            `);
+            $("#pesan").html(`
+              @error('nomer')
+                <span class="invalid-feedback" role="alert">
+                  <strong>{{ $message }}</strong>
+                </span>
+              @enderror
+            `);
+          } else {
+            $('#noId').removeClass("mb-3");
+            $('#noId').html('');
+          }
+      });
+  });
+  function inputAngka(e) {
+    var charCode = (e.which) ? e.which : event.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57)){
+      return false;
+    }
+    return true;
+  }
+</script>
+@yield('script')
+
+@error('id_card')
+  <script>
+    toastr.error("Maaf User ini tidak terdaftar sebagai Guru SMKN 1 Jenangan Ponorogo!");
+  </script>
+@enderror
+@error('guru')
+  <script>
+    toastr.error("Maaf Guru ini sudah terdaftar sebagai User!");
+  </script>
+@enderror
+@error('no_induk')
+  <script>
+    toastr.error("Maaf User ini tidak terdaftar sebagai Siswa SMKN 1 Jenangan Ponorogo!");
+  </script>
+@enderror
+@error('siswa')
+  <script>
+    toastr.error("Maaf Siswa ini sudah terdaftar sebagai User!");
+  </script>
+@enderror
+@if (session('status'))
+  <script>
+    toastr.success("{{ Session('success') }}");
+  </script>
+@endif
+@if (Session::has('error'))
+    <script>
+        toastr.error("{{ Session('error') }}");
+    </script>
+@endif
+
+</body>
+</html>
